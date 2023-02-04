7cc7bb1848cd2bc4a215271bd4117297ec9ab014
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2023-02-04 17:57:37 +0100

    updates

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import profileReducer  from '../features/profileSlice';
+import postReducer from '../features/postSlice';
+
+export const store = configureStore({
+  reducer: {
+    profile: profileReducer,
+    post: postReducer
+  },
+});
