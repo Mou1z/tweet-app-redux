5c8a820ea86a04cd79d4038df5df5f150baabe8b
Aristocrat <73653063+Mou1z@users.noreply.github.com>
2023-02-04 18:53:23 +0100

    Revert "Update 5 files"

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import profileReducer  from '../features/profileSlice';
+import postReducer from '../features/postSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    profile: profileReducer,
+    post: postReducer
+  },
 });
