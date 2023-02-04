81909ef06f2dc4361388347aa470eb1ffc403a23
Aristocrat <mouizghouri77@gmail.com>
2023-02-04 18:01:36 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+function DetailsSection(props) {
+  return (
+    <div>
+      <div>
+        <h2>Profile</h2>
+
+        <label htmlFor="name">Name: </label>
+        <input id="name" />
+        <button>Change</button>
+
+        <p>Posts: </p>
+      </div>
+
+      <div>
+        <h2>App</h2>
+
+        <p>Users: </p>
+        <p>Total Posts: </p>
+      </div>
+    </div>
+  );
+}
+
+function PostsSection(props) {
+  return (
+    <div>
+      <textarea />
+      <button>Post</button>
+    </div>
+  );
+}
+
 function Main(props) {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="main">
+      <DetailsSection />
+      <PostsSection />
+    </div>
+  );
 }
 
 root.render(
